cc4900415f37aec0dd8c1be350d6cefce6ef3804
Giuliano <giulianoandresbasso@gmail.com>
2024-06-05 10:42:15 -0300

    checkear 3er grafico

--- susceptometro.py
+++ susceptometro.py
@@ -11,66 +11,77 @@ from uncertainties import ufloat, unumpy
 import glob
 #%%
 def lector_ciclos(file):
-    
+    with open(file, 'rb') as f:
+        codificacion = chardet.detect(f.read())['encoding']
     data=pd.read_csv(os.path.join(os.getcwd(),file),skiprows=3,
                         usecols=(0,1,2,3,4),
                         decimal='.',engine='python',
+                        encoding=codificacion,
                         names=('F','V','V"','X','X"'))
 
 #                        dtype={'Tiempo_(s)':'float','Campo_(kA/m)':'float','Magnetizacion_(A/m)':'float'})  
     f= pd.Series(data['F']).to_numpy(dtype=float)
     X= pd.Series(data['X']).to_numpy(dtype=float)
     X_2= pd.Series(data['X"']).to_numpy(dtype=float)
-    
+
     return f,X,X_2
-
 #%% Perpendicular
-fig, (ax,ax2) = plt.subplots(nrows=3,figsize=(8,10),constrained_layout=True)
+fig, (ax,ax2,ax3) = plt.subplots(nrows=3,figsize=(7,8),sharex=True,constrained_layout=True)
 for file in glob.glob('**/*P*'):
-    name=file.split('\\')[-1]
+    name=file.split('/')[-1][:-4]
     F,X,X_2=lector_ciclos(file)
-    phi=X_2/x
-    ax.plot(F,X,'.-',label=name)
-    ax2.plot(F,X_2,'.-',label=name)
-
-    print(file)
-ax.set_ylabel(r'$\chi$')
-ax2.set_ylabel(r'$\chi$"')
-for i in [ax,ax2]:
-    i.legend(ncol=2)
-    i.grid()
-    i.set_xlabel('f (Hz)')
-plt.suptitle('Susceptibilidad vs frecuencia - Perpendicular')
-# %%
-#%% Axial
-fig, (ax,ax2) = plt.subplots(nrows=2,figsize=(8,5),constrained_layout=True)
-for file in glob.glob('**/*A*'):
-    name=file.split('\\')[-1]
-    F,X,X_2=lector_ciclos(file)
-    ax.plot(F,X,'.-',label=name)
-    ax2.plot(F,X_2,'.-',label=name)
-    print(file)
-ax.set_ylabel(r'$\chi$')
-ax2.set_ylabel(r'$\chi$"')
-for i in [ax,ax2]:
-    i.legend(ncol=2)
-    i.grid()
-    i.set_xlabel('f (Hz)')
-plt.suptitle('Susceptibilidad vs frecuencia - Axial')
-
-#%% Random
-fig, (ax,ax2) = plt.subplots(nrows=2,figsize=(8.5,5.5),constrained_layout=True)
-for file in glob.glob('**/*R*'):
-    name=file.split('\\')[-1]
-    F,X,X_2=lector_ciclos(file)
+    #phi=X_2/x
     ax.plot(F,X,'o-',label=name)
     ax2.plot(F,X_2,'o-',label=name)
-    print(name)
+    ax3.plot(F,X_2/X,'o-',label=name)
+    print(file)
 ax.set_ylabel(r'$\chi$')
 ax2.set_ylabel(r'$\chi$"')
-for i in [ax,ax2]:
+ax3.set_xlabel('f (Hz)') 
+
+for i in [ax,ax2,ax3]:
     i.legend(ncol=2)
+    i.set_xscale('log')
     i.grid()
-    i.set_xlabel('f (Hz)')
-plt.suptitle('Susceptibilidad vs frecuencia - Random')
+plt.suptitle('Susceptibilidad vs frecuencia - Perpendicular',fontsize=15)
+#%% Axial
+fig, (ax,ax2,ax3) = plt.subplots(nrows=3,figsize=(7,8),sharex=True,constrained_layout=True)
+for file in glob.glob('**/*A*'):
+    name=file.split('/')[-1][:-4]
+    F,X,X_2=lector_ciclos(file)
+    #phi=X_2/x
+    ax.plot(F,X,'o-',label=name)
+    ax2.plot(F,X_2,'o-',label=name)
+    ax3.plot(F,X_2/X,'o-',label=name)
+    print(file)
+ax.set_ylabel(r'$\chi$')
+ax2.set_ylabel(r'$\chi$"')
+ax3.set_xlabel('f (Hz)') 
+
+for i in [ax,ax2,ax3]:
+    i.legend(ncol=2)
+    i.set_xscale('log')
+    i.grid()
+plt.suptitle('Susceptibilidad vs frecuencia - Axial',fontsize=15)
+
+#%% Random
+fig, (ax,ax2,ax3) = plt.subplots(nrows=3,figsize=(7,8),sharex=True,constrained_layout=True)
+for file in glob.glob('**/*R*'):
+    name=file.split('/')[-1][:-4]
+    F,X,X_2=lector_ciclos(file)
+    #phi=X_2/x
+    ax.plot(F,X,'o-',label=name)
+    ax2.plot(F,X_2,'o-',label=name)
+    ax3.plot(F,X_2/X,'o-',label=name)
+    print(file)
+ax.set_ylabel(r'$\chi$')
+ax2.set_ylabel(r'$\chi$"')
+ax3.set_ylabel(r'$\chi$"')
+ax3.set_xlabel('f (Hz)') 
+
+for i in [ax,ax2,ax3]:
+    i.legend(ncol=2)
+    i.set_xscale('log')
+    i.grid()
+plt.suptitle('Susceptibilidad vs frecuencia - Random',fontsize=15)
 # %%
